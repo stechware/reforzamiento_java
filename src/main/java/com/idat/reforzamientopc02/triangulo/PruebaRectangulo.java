fd30ba18d3aa6d6ef9c5e5465d7c0b310482151b
jcst2019 <juansolar_4@hotmail.com>
2025-02-16 23:59:35 -0500

    Clase Rectangulo

---
+++ src/main/java/com/idat/reforzamientopc02/triangulo/PruebaRectangulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.idat.reforzamientopc02.triangulo;
+
+/**
+ *
+ * @author Solaris
+ */
+public class PruebaRectangulo {
+    
+    public static void main(String[] args) {
+        
+        Rectangulo rectangulo1 = new Rectangulo(5,8);
+        Rectangulo rectangulo2 = new Rectangulo(10,20);
+        Rectangulo rectangulo3 = new Rectangulo(1,3);
+        
+        System.out.println(rectangulo1);
+        System.out.println(rectangulo2);
+        System.out.println(rectangulo3);
+        
+    }
+   
+}
