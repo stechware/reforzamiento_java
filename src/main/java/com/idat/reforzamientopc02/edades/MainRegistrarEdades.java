7cc0bb46a0160bd3d788fd25fb8bc058c3bbcbea
jcst2019 <juansolar_4@hotmail.com>
2025-02-23 17:56:42 -0500

    Funcionalidad inicial de la clase Registrar Edades

---
+++ src/main/java/com/idat/reforzamientopc02/edades/MainRegistrarEdades.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.idat.reforzamientopc02.edades;
+
+/**
+ *
+ * @author Solaris
+ */
+public class MainRegistrarEdades {
+    
+    public static void main(String[] args) {
+        
+        RegistrarEdades registro = new RegistrarEdades();
+        registro.registrar(20);
+        registro.registrar(10);
+        registro.registrar(5);
+        registro.registrar(18);
+        registro.registrar(30);
+        
+        
+    }
+    
+}
