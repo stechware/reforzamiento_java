8775fb6a2eb9fe683782b2f75b5fb3698118b2d3
jcst2019 <juansolar_4@hotmail.com>
2025-02-23 18:11:38 -0500

    Funcionalidad final de la clase Registrar Edades

--- src/main/java/com/idat/reforzamientopc02/edades/MainRegistrarEdades.java
+++ src/main/java/com/idat/reforzamientopc02/edades/MainRegistrarEdades.java
@@ -4,6 +4,8 @@
  */
 package com.idat.reforzamientopc02.edades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Solaris
@@ -12,14 +14,34 @@ public class MainRegistrarEdades {
     
     public static void main(String[] args) {
         
-        RegistrarEdades registro = new RegistrarEdades();
+        RegistrarEdades edades = new RegistrarEdades();
+        Scanner scanner = new Scanner(System.in);
+        int edad;
+        boolean bandera=true;
+        
+        /*
         registro.registrar(20);
         registro.registrar(10);
         registro.registrar(5);
         registro.registrar(18);
         registro.registrar(30);
+        */
+        do{
+            System.out.println("Para Salir ingrese 0 ");
+            System.out.print("Ingrese la edad. Considere rango entre 1 y 120 = ");
+            edad = scanner.nextInt();
+            if (edad < 0 || edad > 120){
+                System.out.println("Edad incorrecta . Volver a ingresar la edad."); 
+                continue;
+            }
+            if(edad == 0){
+                bandera = false;
+            }else{
+                edades.registrar(edad);
+            }
+             
+        }while(bandera);
         
-        
-    }
-    
+        scanner.close();
+    }  
 }
